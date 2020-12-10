b14230f6fad05b69749c4ceb52da5f3c3d355a1c
Maria Moalfa <moalfamaria@yahoo.co.uk>
2020-12-10 11:29:22 +0200

    Update tema4.py

--- tema4.py
+++ tema4.py
@@ -4,11 +4,18 @@ from bs4 import BeautifulSoup
 url = "https://en.wikipedia.org/wiki/Python_(genus)"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
+# preluare tabel care se gaseste in dreapta paginii wikipedia legata de specia de sarpe Python
 tab = soup.find("table", {"class":"infobox biota"})
 table_body = tab.find('tbody')
+'''
+pentru a prelua elementele tabelului, in urma verificarii paginii cu inspect
+am observat ca acestea se afla intre tag-ul <tr> </tr>
+ '''
 rows = table_body.find_all('tr')
 
-
+''' 
+afisarea elemenentelor sub forma unor ellemente din matrice 
+'''
 for row in rows:
     data = row.find_all('td')
     data = [elem.text.strip() for elem in data]
@@ -27,6 +34,7 @@ for row in rows:
     if (data and data[0] == 'Genus:'):
         print(data[0] + " " + data[1])
 
+# scrierea intr-un fisier de tip HTML a elementelor aflate intre tag-urile <td> </td>
 with open("output1.html", "w", encoding='utf-8') as file:
     file.write(str(row.find_all('td')))
 
